fb832a51fef58a18e3d65352a0e02b161a83c09e
Jaxon Ross <jaxonlucasross@usf.edu>
2025-10-19 17:26:41 -0400

    added background and form event blocking

--- CP_1.js
+++ CP_1.js
@@ -4,3 +4,9 @@ const MAX = 300;
 const form = document.getElementById("form");
 const feed = document.getElementById("feed");
 const tip  = document.getElementById("tip");
+
+// background click listener (blocked by form clicks)
+document.body.addEventListener("click", () => {});
+
+// stop background from seeing form clicks
+form.addEventListener("click", (e) => e.stopPropagation());
